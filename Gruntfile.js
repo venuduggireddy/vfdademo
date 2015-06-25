4b3c9ea4d35085c64545ad4fe3b9e64abe8b0ebd
anushvarma <anushvarma@yahoo.com>
2015-06-25 16:01:59 -0400

    updated protractor config
    updated protractor config

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
                 args: {
 		          // Arguments passed to the command
 			  baseUrl: '<%= props.baseUrl %>/',
-			  // chromeDriver: '<%= props.chromeDriver %>'
+			  chromeDriver: '<%= props.chromeDriver %>'
 
 		     } // Target-specific arguments
             }
