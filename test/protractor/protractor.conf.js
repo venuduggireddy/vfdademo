4ccb63f3f3c1370dac5e9d84c3137496bfac9960
anushvarma <anushvarma@yahoo.com>
2015-06-25 18:07:08 -0400

    Testing chrome driver

--- test/protractor/protractor.conf.js
+++ test/protractor/protractor.conf.js
@@ -39,7 +39,7 @@ exports.config = {
  */
   directConnect: true,
 
-  // chromeOnly: true,
+  chromeOnly: true,
 
   // -----------------------------------------------------------------
   // Specify the test code that will run.
@@ -94,14 +94,14 @@ exports.config = {
   // Browser and Capabilities: Firefox
   // -----------------------------------------------------------------
  
-
+/*
   capabilities: {
     browserName: 'firefox',
     version: '',
     platform: 'ANY'
   },
   
-
+*/
  
   // -----------------------------------------------------------------
   // Application configuration.
