9365c8c81df67030604e1cf319e8cd151e684782
anushvarma <anushvarma@yahoo.com>
2015-06-26 18:05:10 -0400

    set flag to stop build on failure
    set flag to stop build on failure

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function(grunt) {
     protractor: {
         options: {
             configFile: "test/protractor/protractor.conf.js", // Default config file
-            keepAlive: true, // If false, the grunt process stops when the test fails.
+            keepAlive: false, // If false, the grunt process stops when the test fails.
             noColor: false, // If true, protractor will not use colors in its output.
             args: {
             }
