80226eae8d75a60ab7d72d8f8114baf773d842fc
anushvarma <anushvarma@yahoo.com>
2015-06-29 18:07:48 -0400

    Updated E2E Test Script

--- test/protractor/spec/fdademo-e2e-test.spec.js
+++ test/protractor/spec/fdademo-e2e-test.spec.js
@@ -15,25 +15,15 @@
  protractor: false
  */
 
-describe('Example:', function () {
+describe('FDA Demo Integration Tests:', function () {
     'use strict';
 
     beforeEach(function () {
-        // Load up a view and wait for it to be done with its rendering and epicycles.
-       // browser.waitForAngular();
+      // Load up a view and wait for it to be done with its rendering and epicycles.
+      // return browser.ignoreSynchronization = true;
     });
 
-/*
-    it('Verify Home Page', function () {
-        browser.get('');
-        var continueButton = browser.findElement(by.cssContainingText('.btn-primary', 'Continue'));
-        expect(continueButton.isDisplayed()).toBe(true);
-        //expect(element.getText()).toBe('Continue');
 
-	
-    });
-
-*/
     it('Verify navigation to Search Page', function () {
         browser.get('search/#/');
         var element = browser.findElement(by.css('[ng-click="searchData()"]'));
@@ -41,28 +31,55 @@ describe('Example:', function () {
         expect(element.getText()).toBe('Search');
     });
 
-/*
-    it('Verify Map View', function () {
-	browser.get('search/#/mapSearch');
-        var element = browser.findElement(by.model('dateRange'));
+
+    it('Verify Search Criteria: Map View', function () {
+	browser.get('search/#/');
+	browser.waitForAngular();
+        var element = browser.findElement(by.model('searchCriteria.startDate'));
         expect(element.isDisplayed()).toBe(true);
-        var element = browser.findElement(by.model('searchCriteria.drug'));
+ 	var element = browser.findElement(by.model('searchCriteria.endDate'));
         expect(element.isDisplayed()).toBe(true);
-	var element = browser.findElement(by.model('searchCriteria.device'));
-        expect(element.isDisplayed()).toBe(true);
-	var element = browser.findElement(by.model('searchCriteria.food'));
+	var element = browser.findElement(by.model('searchCriteria.keyTerm'));
         expect(element.isDisplayed()).toBe(true);
     });
 
-    it('Verify Search Input', function () {
-        browser.get('search/#/');
-        var element = browser.findElement(by.model('searchCriteria.states'));
+    it('Verify Search Results - Map View', function () {
+	browser.get('search/#/');
+	browser.waitForAngular();
+        var element = browser.findElement(by.model('searchCriteria.keyTerm'));
+        element.sendKeys('pain')
+
+        var searchButton = browser.findElement(by.css('[ng-click="searchData()"]'));
+        searchButton.click();
+
+        browser.waitForAngular();
+	var serachResults = browser.findElements(by.repeater('y in searchCriteria.selectedRecall'));
+
+        console.log('Search String Entered = Pain');
+
+     	serachResults.then(function(result){
+            console.log('Search Result count ='+result.length);
+            expect(result.length).toBeGreaterThan(0);
+        });
+
+    });
+
+    it('Verify Search - List View: List View', function () {
+	browser.get('search/#/listSearch');
+	browser.waitForAngular();
+        var element = browser.findElement(by.model('searchCriteria.startDate'));
+        expect(element.isDisplayed()).toBe(true);
+ 	var element = browser.findElement(by.model('searchCriteria.endDate'));
+        expect(element.isDisplayed()).toBe(true);
+	var element = browser.findElement(by.model('searchCriteria.keyTerm'));
         expect(element.isDisplayed()).toBe(true);
     });
 
 
-    it('Verify Search Results', function () {
-        var element = browser.findElement(by.model('symptom'));
+    it('Verify Search Results - List View', function () {
+	browser.get('search/#/listSearch');
+	browser.waitForAngular();
+        var element = browser.findElement(by.model('searchCriteria.keyTerm'));
         element.sendKeys('pain')
 
         var searchButton = browser.findElement(by.css('[ng-click="searchData()"]'));
@@ -70,24 +87,28 @@ describe('Example:', function () {
 
         browser.waitForAngular();
 
-        var searchList = browser.findElements(by.repeater('y in products.medicinalproduct'));
-        var searchList2 = browser.findElements(by.repeater('x in products.reaction'));
+        var statesSelected = browser.findElements(by.repeater('y in searchCriteria.states'));
+        var recallItems = browser.findElements(by.repeater('x in searchCriteria.selectedRecall'));
+	var serachResults = browser.findElements(by.repeater('y in products.results'));
 
         console.log('Search String Entered = Pain');
 
-        searchList.then(function(result){
-            console.log('product count ='+result.length);
+        statesSelected.then(function(result){
+            console.log('State selected count ='+result.length);
             expect(result.length).toBeGreaterThan(0);
         });
 
-        searchList2.then(function(result){
-            console.log('Reaction count ='+result.length);
+        recallItems.then(function(result){
+            console.log('Reaction items count ='+result.length);
+            expect(result.length).toBeGreaterThan(0);
+        });
+
+	serachResults.then(function(result){
+            console.log('Search Result count ='+result.length);
             expect(result.length).toBeGreaterThan(0);
         });
 
     });
-
-*/
 
 });
 
