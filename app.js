5b39c2e51a900bc5d2c6a4ea4a7105eeb7c4d35e
Venu Duggireddy <venu.dvmr@gmail.com>
2015-07-01 16:09:08 -0400

    added new config file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /// <reference path="typings/node/node.d.ts"/>
+/// <reference path="typings/express/express.d.ts" />
+/// <reference path="typings/express/express-middleware.d.ts"/>
 
 /*
   author: venu duggireddy
