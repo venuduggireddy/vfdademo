68769aa750ea4d0acf09be383113268658c912fb
anushvarma <anushvarma@yahoo.com>
2015-07-02 17:15:20 -0400

    updated Scripts
    updated Scripts

--- test/protractor/spec/fdademo-e2e-test.spec.js
+++ test/protractor/spec/fdademo-e2e-test.spec.js
@@ -15,22 +15,25 @@
  protractor: false
  */
 
-describe('FDA Demo Integration Tests: Test Suite 1', function () {
+describe('FDA Demo Integration Tests: Test Suite 1 - Home Page', function () {
     beforeEach(function () {
 	return browser.ignoreSynchronization = true;
     });
 
     it('Verify navigation to Home Page', function () {
 	browser.get('');
+	browser.driver.sleep(3000);
+	var agreeButton = browser.findElement(by.css('.form-group a'));
+	expect(agreeButton.isDisplayed()).toBe(true);
+	agreeButton.click();
     });
 });
 
 
-describe('FDA Demo Integration Tests: Test Suite 2', function () {
-    'use strict';
-
+describe('FDA Demo Integration Tests: Test Suite 2 - Map View', function () {
+   
     beforeEach(function () {
-	browser.ignoreSynchronization = false;	
+	return browser.ignoreSynchronization = false;	
     });
 
    it('Verify navigation to Map View', function () {
@@ -54,20 +57,49 @@ describe('FDA Demo Integration Tests: Test Suite 2', function () {
 	});
     });
 
-    it('Verify navigation to List View', function () {
+
+});
+
+describe('FDA Demo Integration Tests: Test Suite 3 - List View', function () {
+   
+    beforeEach(function () {
         browser.get('search/#/listSearch');
 	browser.waitForAngular();
+    });
+
+
+    it('Verify navigation to List View', function () {
         var element = browser.findElement(by.model('searchCriteria.keyTerm'));
         expect(element.getText()).toBe('');
 
         browser.findElements(by.repeater('y in searchCriteria.states')).then(function(states) {
 	    var noOfStates = states.length;
-	    console.log(noOfStates);
+	    console.log('Number of States=' + noOfStates);
 	    expect(noOfStates).toBeGreaterThan(0);
    
 	});
         
     });
 
-});
+    it('Verify Required Search Criteria', function () {
+        var startDate = browser.findElement(by.model('searchCriteria.startDate'));
+        expect(startDate.getText()).toNotBe(null);
+	var endDate = browser.findElement(by.model('searchCriteria.endDate'));
+        expect(endDate.getText()).toNotBe(null);
 
+        browser.findElements(by.repeater('y in searchCriteria.states')).then(function(states) {
+	    var noOfStates = states.length;
+	    console.log('Number of States=' + noOfStates);
+	    expect(noOfStates).toBeGreaterThan(0);
+	});
+        
+    });
+
+    it('Verify Default Search Results', function () {
+      	browser.findElements(by.repeater('y in products.results')).then(function(results) {
+	    var noOfResults = results.length;
+	    console.log('Number of Results=' + noOfResults);
+	    expect(noOfResults).toBeGreaterThan(0);
+	});
+    });
+});
